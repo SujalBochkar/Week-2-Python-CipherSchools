a08fb6fce7b587de94d4c177b4709415d82e730b
SujalBochkar <Sujalbochkar@gmail.com>
2022-12-08 19:59:47 +0530

    Create Class075-FunPractice-CipherSchools.py

---
+++ Class075-FunPractice-CipherSchools.py
@@ -0,0 +1,14 @@
+# def last(name):
+#     return name[-1]
+# print(last(input("Enter the String : ")))
+
+# def ooe(x):
+#     if x%2 == 0:
+#         return"even"
+#     return "odd"
+# x = int(input())
+# print(ooe(x))
+
+def eod(x):
+    return x%2==0
+print(eod(int(input())))
