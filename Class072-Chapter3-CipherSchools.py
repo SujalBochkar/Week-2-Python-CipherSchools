9f4145bebfce75f6652e37dea21d7d75bbdf039c
SujalBochkar <Sujalbochkar@gmail.com>
2022-12-08 19:59:31 +0530

    Update Class072-Chapter3-CipherSchools.py

---
+++ Class072-Chapter3-CipherSchools.py
@@ -0,0 +1,26 @@
+# if statement
+name = (input())
+if name == 1:
+    print("hii")
+
+# while
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+
+# for Loop
+for i in range(1,11,2):
+    print(i)
+
+# break keyword
+for i in range (1,11):
+    if i ==5:
+        break
+    print(i)
+
+# continue keyword
+for i in range (1,11):
+    if i ==5:
+        continue
+    print(i)
