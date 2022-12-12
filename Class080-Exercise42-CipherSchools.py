e09c48bae66294cbe8ccd3c3148c1b8d917fd10d
SujalBochkar <Sujalbochkar@gmail.com>
2022-12-12 22:21:17 +0530

    Update

---
+++ Class080-Exercise42-CipherSchools.py
@@ -0,0 +1,11 @@
+
+# Define is_palindrome function that take one word in string as input
+# and return True if it is palindrome else return False
+# palindrome - word that reads same backwards as forwards
+# example
+# is_palindrome ("madam") 
+# isindrome ("naman") Tru
+# is_palindrome ("horse")
+# logic (algorithm) 
+# step 1 -> reverse the string
+# #step 2 -> compare reversed string with original string
