40e3d9cd99ee5aa821184d490532d54649902cd9
SujalBochkar <Sujalbochkar@gmail.com>
2022-12-12 22:22:01 +0530

    Update

---
+++ Class083-Default-CipherSchools.py
@@ -0,0 +1,6 @@
+# Default Paramenters
+def user_info(first_name, last_name, age = 17): 
+    print (f"Your first name is {first_name}") 
+    print (f"Your last name is {last_name}") 
+    print (f"you age is {age}")
+user_info('Sujal', 'Bochkar', 18)
